97acc4f27fc6abd06e2d247d048532979e342a4e
Skylar <skylarblessings5@gmail.com>
2023-05-23 02:59:33 +0300

    task

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,13 @@
 import re
 
-my_pattern = r""
+my_pattern = r"It's such a lovely day today."
 my_regex = re.compile(my_pattern)
 
+def test_my_regex():
+    assert my_regex.match("It's such a lovely day today.")
+
+    # Add the following test case
+    assert my_regex.fullmatch("Some weather we're having today, huh?")
+
+if __name__ == "__main__":
+    test_my_regex()
